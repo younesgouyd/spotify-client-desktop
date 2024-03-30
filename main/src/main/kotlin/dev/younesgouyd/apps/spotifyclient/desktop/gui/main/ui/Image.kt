c74ca22a6f58ddc86543cdef171296c20c0feead
Younes Gouyd <younes.gouyd@gmail.com>
2024-03-30 17:26:32 +0000

    limit cache size

--- main/src/main/kotlin/dev/younesgouyd/apps/spotifyclient/desktop/gui/main/ui/Image.kt
+++ main/src/main/kotlin/dev/younesgouyd/apps/spotifyclient/desktop/gui/main/ui/Image.kt
@@ -8,51 +8,37 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asSkiaBitmap
 import androidx.compose.ui.res.loadImageBitmap
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.net.URI
+import java.time.Instant
 
 typealias ImageUrl = String
-
-private val cache = mutableMapOf<ImageUrl, ImageBitmap>()
 
 @Composable
 fun Image(
     modifier: Modifier = Modifier,
-    url: ImageUrl?,
+    url: ImageUrl?
 ) {
     var loading by remember { mutableStateOf(true) }
-    var img by remember { mutableStateOf<ImageBitmap?>(null) }
+    var image by remember { mutableStateOf<ImageBitmap?>(null) }
 
     if (url == null) {
         BrokenImage(modifier)
     } else {
         LaunchedEffect(Unit) {
-            withContext(Dispatchers.IO) {
-                loading = true
-                val fromCache = cache[url]
-                if (fromCache == null) {
-                    try {
-                        URI(url).toURL().openStream().use {
-                            val imageBitmap = loadImageBitmap(it)
-                            img = imageBitmap
-                            cache += url to imageBitmap
-                        }
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                } else {
-                    img = fromCache
-                }
-                loading = false
-            }
+            loading = true
+            image = Cache.get(url)
+            loading = false
         }
 
         when (loading) {
             true -> LoadingImage(modifier)
             false -> {
-                img?.let {
+                image?.let {
                     androidx.compose.foundation.Image(
                         modifier = modifier,
                         bitmap = it,
@@ -65,7 +51,19 @@ fun Image(
 }
 
 @Composable
-fun BrokenImage(
+private fun LoadingImage(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun BrokenImage(
     modifier: Modifier = Modifier
 ) {
     androidx.compose.foundation.Image(
@@ -75,14 +73,63 @@ fun BrokenImage(
     )
 }
 
-@Composable
-private fun LoadingImage(
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier,
-        contentAlignment = Alignment.Center
-    ) {
-        CircularProgressIndicator()
+private object Cache {
+    private const val MAX_CACHE_SIZE = 100 * 1024 * 1024
+
+    private val cache = mutableMapOf<ImageUrl, Image>()
+    private val mutex = Mutex()
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private var cacheSize = 0
+
+    suspend fun get(url: ImageUrl): ImageBitmap {
+        return withContext(Dispatchers.IO) {
+            val fromCache = cache[url]
+            if (fromCache == null) {
+                mutex.withLock {
+                    val fromCache2 = cache[url]
+                    if (fromCache2 == null) {
+                        URI(url).toURL().openStream().use { inputStream ->
+                            val imageBitmap = loadImageBitmap(inputStream)
+                            val image = Image(imageBitmap)
+                            add(url to image)
+                            while (cacheSize > MAX_CACHE_SIZE) {
+                                val leastImportant = cache.minByOrNull { it.value.lastUsed }
+                                if (leastImportant != null) {
+                                    remove(leastImportant.toPair())
+                                }
+                            }
+                            image.bitmap
+                        }
+                    } else {
+                        fromCache2.updateLastUsed()
+                        fromCache2.bitmap
+                    }
+                }
+            } else {
+                fromCache.updateLastUsed()
+                fromCache.bitmap
+            }
+        }
+    }
+
+    private suspend fun add(entry: Pair<ImageUrl, Image>) {
+        scope.launch {
+            cache += entry
+            cacheSize += entry.second.byteSize
+        }.join()
+    }
+
+    private suspend fun remove(entry: Pair<ImageUrl, Image>) {
+        scope.launch {
+            cache.remove(entry.first)
+            cacheSize -= entry.second.byteSize
+        }.join()
+    }
+
+    private data class Image(val bitmap: ImageBitmap) {
+        var lastUsed = Instant.now().toEpochMilli()
+        val byteSize = bitmap.asSkiaBitmap().computeByteSize()
+
+        fun updateLastUsed() { lastUsed = Instant.now().toEpochMilli() }
     }
 }
